ee840deafe5f5eac4a620d75e3fee457a98672ba
Roman <alprem2016@yandex.ru>
2026-09-13 12:23:30 +0300

    add permissions from post and comment

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,14 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets
+
+from posts.models import Comment, Group, Post
+
+from .permissions import PostAndCommentPermission
+from .serializers import CommetSerializer, GroupSerializer, PostSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.select_related('author').all()
+    serializer_class = PostSerializer
+    permission_classes = (PostAndCommentPermission,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
