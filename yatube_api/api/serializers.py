21ed1333ff62bef25ed9e664b83037dad72c1e88
Roman <alprem2016@yandex.ru>
2026-09-13 14:32:15 +0300

    fix flake8

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,8 +2,10 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from posts.models import Comment, Follow, Group, Post
-from .validators import (validate_follow_not_self,
-                                       validate_follow_unique)
+from .validators import (
+    validate_follow_not_self,
+    validate_follow_unique
+)
 
 User = get_user_model()
 
