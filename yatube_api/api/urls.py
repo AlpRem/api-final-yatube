e70907d7592ffec49ce793a896dd29375dab8127
Roman <alprem2016@yandex.ru>
2026-09-13 13:07:53 +0300

    add comment

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,13 +1,32 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import PostViewSet
+from .views import PostViewSet, CommentViewSet
 
 router = DefaultRouter()
 router.register('posts', PostViewSet)
+comment_list = CommentViewSet.as_view({'get': 'list', 'post': 'create'})
+comment_detail = CommentViewSet.as_view(
+    {
+        'get': 'retrieve',
+        'put': 'update',
+        'patch': 'partial_update',
+        'delete': 'destroy'
+    }
+)
 
 urlpatterns = [
     path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
     path('v1/', include(router.urls)),
+    path(
+        'v1/posts/<int:post_id>/comments/',
+        comment_list,
+        name='comment-list'
+    ),
+    path(
+        'v1/posts/<int:post_id>/comments/<int:comment_id>/',
+        comment_detail,
+        name='comment-detail'
+    ),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -14,3 +14,21 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (PostAndCommentPermission,)
+    lookup_url_kwarg = 'comment_id'
+
+    def get_queryset(self):
+        return Comment.objects.select_related(
+            'author',
+            'post'
+        ).filter(post_id=self.kwargs['post_id'])
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            post_id=self.kwargs['post_id']
+        )
